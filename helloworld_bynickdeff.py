230125b77d6f5fd6560f2ae900f1b3b9b9d71d83
IFK1 <114928043+IFK1@users.noreply.github.com>
2023-08-28 16:10:58 +0200

    Create helloworld_bynickdeff.py

---
+++ helloworld_bynickdeff.py
@@ -0,0 +1,38 @@
+char = "H"
+print(char, end='')
+
+char = "e"
+print(char, end='')
+
+char = "l"
+print(char, end='')
+
+char = "l"
+print(char, end='')
+
+char = "o"
+print(char, end='')
+
+char = ","
+print(char, end='')
+
+char = " "
+print(char, end='')
+
+char = "w"
+print(char, end='')
+
+char = "o"
+print(char, end='')
+
+char = "r"
+print(char, end='')
+
+char = "l"
+print(char, end='')
+
+char = "d"
+print(char, end='')
+
+char = "!"
+print(char)
